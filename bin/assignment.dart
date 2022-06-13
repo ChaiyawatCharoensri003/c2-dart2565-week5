ada45fd0129b18f62ae6fd8675c613a2a336aab7
wayusangchun <wayu.best.2549@gmail.com>
2022-06-13 12:31:25 +0700

    first commit

---
+++ bin/assignment.dart
@@ -0,0 +1,18 @@
+main() {
+  print("Example of Assignment Operators");
+  int a = 10;
+  int b = 5;
+  //a += b;
+  //print("a + b = $a");//Asp and Asign
+  //a -= b;
+  //print("a -= b = $a");
+
+  a *= b;
+  print("a *= b = $a");//a: a = a*b
+
+ a ~/= b;
+ print("a ~/b = $a"); //a = a~/b 
+
+ a %= b;
+ print("a %= b = $a"); //a = a%b
+}
